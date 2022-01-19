bf25b528636124619991018fd3ac836af4f0187e
Jett <premiumconsulting@jettdembin.dev>
2022-01-18 23:01:57 -0500

    working meals per day function for 3

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const percentFat = document.getElementById('percentage-f');
 const regex = /^(0|[1-9]\d*)(\.\d+)?$/;
 
 caloriesTotal.textContent = '2000';
+caloricCarb.textContent = "0";
+caloricProtein.textContent = "0";
+caloricFat.textContent = "0";
+percentCarb.textContent = "0";
+percentProtein.textContent = "0";
+percentFat.textContent = "0";
+
+const adjustOptions = document.querySelectorAll('.adjust-selection');
 
 //listen to weight input update
 weightInput.addEventListener('keypress', function(e) {
@@ -19,6 +27,11 @@ weightInput.addEventListener('keypress', function(e) {
     if (e.key === "Enter") {
         if (regex.test(e.target.value)) {
             updateWeight(e);
+            adjustOptions.forEach(option => {
+                option.removeEventListener('click', adjustMeals);
+                option.addEventListener('click', adjustMeals);
+                console.log(option)
+            });
         } else {
             alert("Please only enter integers. Only one decimal is allowed")
         }
@@ -27,11 +40,18 @@ weightInput.addEventListener('keypress', function(e) {
 
 //listen to click for calculate
 calculate.forEach(btn => {
-    btn.addEventListener('click', calculateCal);
+    btn.addEventListener('click', (e)=> {
+        calculateCal(e);
+    });
 });
 
 //function to calculate calories
 function calculateCal(e) {
+    adjustOptions.forEach(option => {
+        option.removeEventListener('click', adjustMeals);
+        option.addEventListener('click', adjustMeals);
+        console.log(option)
+    });
     if (e.target.id === 'cut') {
         const calories = Math.round(weightInput.value * 12);
         caloriesTotal.textContent = calories;
@@ -84,3 +104,48 @@ function updateWeight(e) {
     // weight = e.target.value;
     weightInput.setAttribute('value', e.target.value);
 };
+
+// //function to listen adjust meals
+// adjustOptions.forEach(option => {
+//     option.addEventListener('click', adjustMeals);
+// });
+
+const calorieHeader = document.querySelector(".calories-header h2");
+
+let counter = 0;
+
+//function to show values of macros per meal
+function adjustMeals(e) {
+    if (e.target.textContent === "All") {
+        calorieHeader.textContent = "Calories Per Day";
+    } else {
+        calorieHeader.textContent = "Calories Per Meal";
+    }
+    if (e.target.textContent === "3") {
+        calorieHeader.textContent = "Calories Per Meal";
+        let total = Number(caloriesTotal.textContent)/3;
+        let carb = Number(caloricCarb.textContent)/3;
+        let protein = Number(caloricFat.textContent)/3;
+        let fat = Number(caloricProtein.textContent)/3;
+
+        caloriesTotal.textContent = Math.round(total);
+        caloricCarb.textContent = Math.round(carb);
+        caloricProtein.textContent = Math.round(protein);
+        caloricFat.textContent = Math.round(fat);
+
+        e.target.removeEventListener('click', adjustMeals);
+    }
+    
+    if (e.target.textContent === "4") {
+        caloriesTotal.textContent = Math.round(Number(caloriesTotal.textContent)/4);
+        caloricCarb.textContent = Math.round(Number(caloricCarb.textContent)/4);
+        caloricProtein.textContent = Math.round(Number(caloricProtein.textContent)/4);
+        caloricFat.textContent = Math.round(Number(caloricFat.textContent)/4);
+    }
+    if (e.target.textContent === "5") {
+        caloriesTotal.textContent = Math.round(Number(caloriesTotal.textContent)/5);
+        caloricCarb.textContent = Math.round(Number(caloricCarb.textContent)/5);
+        caloricProtein.textContent = Math.round(Number(caloricProtein.textContent)/5);
+        caloricFat.textContent = Math.round(Number(caloricFat.textContent)/5);
+    }
+}
